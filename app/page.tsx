f61e95f564ba06faafc63f59881197fd89dcdf2b
Flibouche <kevin.pfiffer2@gmail.com>
2025-02-10 21:02:02 +0100

    🐛 FIX: Remove Mantine.dev library.

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,22 @@ import { auth } from "@/lib/auth";
 
 export default async function Home() {
     const session = await auth();
+
     return (
-        <div className="min-h-screen bg-gray-500 flex flex-col items-center justify-center -mt-16 text-black">
+        <div className="min-h-screen flex flex-col items-center justify-center -mt-16 text-white">
             <div className="flex flex-col justify-center items-center m-4">
-                <h1 className="text-3xl my-3">Hey, this is the home page</h1>
+                <h1>Hey, this is the home page</h1>
             </div>
             {session ? (
-                <h1 className="text-3xl my-2">
+                <h2 className="text-3xl my-2">
                     Welcome, {session?.user?.name}
-                </h1>
+                </h2>
             ) : (
-                <h1 className="text-3xl my-2">
-                    Welcome, {session?.user?.email}
-                </h1>
+                <div>
+                    <h2 className="text-3xl my-2">
+                        Welcome !
+                    </h2>
+                </div>
             )}
         </div>
     );
