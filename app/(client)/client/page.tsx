b4e8144c12d3ea9957f363a12b063531e3eef77e
Flibouche <kevin.pfiffer2@gmail.com>
2025-02-12 19:03:16 +0100

    NEW: Feature : add view client
    Add a title and a h2.

---
+++ app/(client)/client/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Client = () => {
+    return (
+        <div>
+            <h1>Client</h1>
+            <h2>Wip</h2>
+        </div>
+    )
+}
+
+export default Client
