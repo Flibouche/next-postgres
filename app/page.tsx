21e069703d74725fa51950e468d26f6340f172e4
Flibouche <kevin.pfiffer2@gmail.com>
2025-02-15 15:40:55 +0100

    IMPROVE: add user informations to the homepage
    - Add email
    - Add role

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,12 @@ export default async function Home() {
                 <h1>Hey, this is the home page</h1>
             </div>
             {session ? (
-                <h2 className="text-3xl my-2">
-                    Welcome, {session?.user?.name}
-                </h2>
+                <>
+                    <h2 className="text-3xl my-2">
+                        Welcome, {session?.user?.name}, your role is : {session?.user?.role}
+                    </h2>
+                    <span>Your email is : {session?.user?.email}</span>
+                </>
             ) : (
                 <div>
                     <h2 className="text-3xl my-2">
